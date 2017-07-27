2e4793663fa7779d3d14e4d11edb730017c4d7f9
Yuwei Zhou <yuwzho@microsoft.com>
2017-07-27 11:15:14 +0800

    docker util

--- core/src/main/java/io/fabric8/maven/core/util/SecretConstants.java
+++ core/src/main/java/io/fabric8/maven/core/util/SecretConstants.java
@@ -3,7 +3,7 @@ package io.fabric8.maven.core.util;
 public class SecretConstants {
     public static String FOLDER_NAME = "secrets";
     public static String KIND = "Secret";
-    public static String DOCKER_CONFIG_TYPE = "kubernetes.io/dockerconfigjson";
-    public static String DOCKER_DATA_KEY = ".dockerconfigjson";
+    public static String DOCKER_CONFIG_TYPE = "kubernetes.io/dockercfg";
+    public static String DOCKER_DATA_KEY = ".dockercfg";
     public static String API_VERSION = "v1";
 }
